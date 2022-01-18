e1a58b8922c29f6f89d6da22ad97614c0d64fe6c
Daniel Diniz <daniel.diniz1@icloud.com>
2022-01-18 08:44:59 -0300

    commit inicial, programa web básico!

---
+++ src/main/java/br/com/alura/mvc/modi/controller/HomeController.java
@@ -0,0 +1,40 @@
+package br.com.alura.mvc.modi.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import br.com.alura.mvc.modi.model.StatusPedido;
+import br.com.alura.mvc.modi.repository.PedidoRepository;
+
+@Controller
+@RequestMapping("/home")
+public class HomeController {
+	
+	@Autowired
+	private PedidoRepository pedidoRepository;
+	
+	@GetMapping
+	public String home(Model model) {
+//		List<Pedido> pedidos = pedidoRepository.findAll();
+//		model.addAttribute("pedidos",pedidos);
+		model.addAttribute("pedidos",pedidoRepository.findAll());
+		return "home";
+	}
+	
+	@GetMapping("/{status}")
+	public String porStatus(@PathVariable("status")String status,Model model) {
+		model.addAttribute("pedidos",pedidoRepository.findByStatus(StatusPedido.valueOf(status.toUpperCase())));
+		model.addAttribute("status", status);
+		return "home";
+	}
+	
+	@ExceptionHandler(IllegalArgumentException.class)
+	public String onError() {
+		return "redirect:/home";
+	}
+}
---
+++ src/main/java/br/com/alura/mvc/modi/model/StatusPedido.java
@@ -0,0 +1,5 @@
+package br.com.alura.mvc.modi.model;
+
+public enum StatusPedido {
+	AGUARDANDO, APROVADO, ENTREGUE;
+}
